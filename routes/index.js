0513b62f6d743169cd0d3d43b6894d2f1167bfe4
Philip Bjerknes <philleif@Philips-iMac-Pro.local>
2018-03-19 16:50:31 -0400

    ui updates

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+"use strict"
+
+const express = require("express")
+const router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get("/", function(req, res, next) {
+  res.render("index")
+})
 
-module.exports = router;
+/* JOIN flow. */
+router.get("/join", function(req, res, next) {
+  res.render("join")
+})
+
+module.exports = router
