d772ce1f05ba2f06c9c9ca3e0d48845970f6e362
Philip Bjerknes <philleif@Philips-iMac-Pro.local>
2018-03-22 17:36:54 -0400

    working local auth

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 "use strict"
 
+require("dotenv").config()
+
+const passport = require("passport")
 const express = require("express")
 const router = express.Router()
 
@@ -8,9 +11,43 @@ router.get("/", function(req, res, next) {
   res.render("index")
 })
 
-/* JOIN flow. */
-router.get("/join", function(req, res, next) {
-  res.render("join")
+/* User Homepage. */
+router.get("/dashboard", function(req, res) {
+  res.render("dashboard", {
+    user: req.user // get the user out of session and pass to template
+  })
+})
+
+/* Authentication. */
+router.post(
+  "/join",
+  passport.authenticate("local-signup", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/join",
+    failureFlash: true
+  })
+)
+
+router.get("/join", (req, res) => {
+  res.render("join", { message: req.flash("signupMessage") })
+})
+
+router.get("/login", (req, res) => {
+  res.render("login", { message: req.flash("loginMessage") })
+})
+
+router.post(
+  "/login",
+  passport.authenticate("local-login", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/login",
+    failureFlash: true
+  })
+)
+
+router.get("/logout", (req, res) => {
+  req.logout()
+  res.redirect("/")
 })
 
 module.exports = router
