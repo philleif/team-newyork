db618724c1f59ef1fcc345fa44a652748d7efc89
Philip Bjerknes <philleif@Philips-iMac-Pro.local>
2018-03-22 17:39:15 -0400

    authenticate dashboard

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,18 @@ const passport = require("passport")
 const express = require("express")
 const router = express.Router()
 
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next()
+  res.redirect("/")
+}
+
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.render("index")
 })
 
 /* User Homepage. */
-router.get("/dashboard", function(req, res) {
+router.get("/dashboard", isLoggedIn, function(req, res) {
   res.render("dashboard", {
     user: req.user // get the user out of session and pass to template
   })
