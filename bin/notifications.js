1ec21a0f48bb6c5676d701ea9385ae1ad6f1434d
Philip Bjerknes <philleif@Philips-iMac-Pro.local>
2018-04-08 10:52:35 -0400

    update notification timing

--- bin/notifications.js
+++ bin/notifications.js
@@ -5,23 +5,11 @@ const notifications = require("../lib/notifications")
 const agenda = require("../lib/agenda")
 
 const run = async () => {
-  /*
-  let user = await db.User.findOne({})
-  let meeting = await db.Meeting.findOne({})
-
-  // send an event notification
-  await notifications.meeting(user, meeting)
-  user.notifications.meetings.push(meetings.id)
-  user.lastMessage = new Date()
-  await user.save()
-  */
-
   agenda.on("ready", async () => {
     await db.AgendaJob.remove({})
 
-    agenda.every("10 seconds", "send letter notifications")
-    agenda.every("10 seconds", "send meeting notifications")
-
+    agenda.every("1 hour", "send letter notifications")
+    agenda.every("1 hour", "send meeting notifications")
 
     agenda.start()
   })
