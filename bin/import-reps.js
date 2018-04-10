8d8c65f9031b56dbcf354ccdee942b8806ef1b4a
Philip Bjerknes <philleif@Philips-iMac-Pro.local>
2018-04-10 07:53:17 -0400

    combine rep import scripts

--- bin/import-reps.js
+++ bin/import-reps.js
@@ -2,6 +2,7 @@
 
 const db = require("../lib/db")
 const googleCivic = require("../lib/google-civic")
+const city = require("../lib/city-council")
 
 const run = async () => {
   try {
@@ -28,6 +29,35 @@ const run = async () => {
         await rep.save()
       }
     }
+
+    let districts = await city.fetchDistricts()
+
+    for (let district of districts.districts) {
+      let representative = new db.Representative({
+        name: district["member-name"],
+        phones: [district["phone"]],
+        district: district["district-number"],
+        emails: [district["email"]],
+        channels: [{ Type: "twitter", id: district["twitter"] }],
+        portrait: district["member-banner"],
+        urls: [district["website"]],
+        office: "City Council"
+      })
+
+      await representative.save()
+
+      let d = new db.District({
+        name: district["district-name"],
+        id: district["id"],
+        number: district["district-number"],
+        borough: district["district-borough"],
+        neighborhoods: district["neighborhoods"].split(", ")
+      })
+
+      await d.save()
+    }
+
+    console.log("Done.")
   } catch (error) {
     throw error
   }
